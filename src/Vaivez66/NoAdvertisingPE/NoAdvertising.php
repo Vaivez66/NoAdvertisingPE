233f4da526fa13f5fefa0406e78f0adeb6974d6b
Reziq Vavai <kakakvavai66@gmail.com>
2016-02-09 16:27:38 +0700

    Update NoAdvertising.php

--- src/Vaivez66/NoAdvertisingPE/NoAdvertising.php
+++ src/Vaivez66/NoAdvertisingPE/NoAdvertising.php
@@ -14,123 +14,123 @@ use pocketmine\utils\TextFormat as TF;
 
 class NoAdvertising extends PluginBase implements Listener{
 
-	public $cfg;
-	private $format;
+    public $cfg;
+    private $format;
 
     public function onEnable(){
-		$this->saveDefaultConfig();
-		$this->format = new NoAdvertisingFormat($this);
-		$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
-		$this->getServer()->getLogger()->info(TF::GREEN . "NoAdvertisingPE is ready!");
-		$this->getServer()->getPluginManager()->registerEvents(new NoAdvertisingListener($this), $this);
-		$this->getCommand("na")->setExecutor(new NoAdvertisingCommand($this));
-	}
+	$this->saveDefaultConfig();
+	$this->format = new NoAdvertisingFormat($this);
+	$this->cfg = new Config($this->getDataFolder() . "config.yml", Config::YAML, array());
+	$this->getServer()->getLogger()->info(TF::GREEN . "NoAdvertisingPE is ready!");
+	$this->getServer()->getPluginManager()->registerEvents(new NoAdvertisingListener($this), $this);
+	$this->getCommand("na")->setExecutor(new NoAdvertisingCommand($this));
+    }
 
-	/**
-	 * @return array
-	 */
+    /**
+     * @return array
+     */
 
-	public function getDomain(){
-		$domain = (array) $this->cfg->get("domain");
-		return $domain;
-	}
+    public function getDomain(){
+	$domain = (array) $this->cfg->get("domain");
+	return $domain;
+    }
 
-	/**
-	 * @return array
-	 */
+    /**
+     * @return array
+     */
 
-	public function getAllowedDomain(){
-		$allowed = (array) $this->cfg->get("allowed.domain");
-		return $allowed;
-	}
+    public function getAllowedDomain(){
+	$allowed = (array) $this->cfg->get("allowed.domain");
+	return $allowed;
+    }
 
-	/**
-	 * @return mixed
-	 */
+    /**
+     * @return mixed
+     */
 
-	public function getType(){
-		return $this->cfg->get("type");
-	}
+    public function getType(){
+	return $this->cfg->get("type");
+    }
 
-	/**
-	 * @return mixed
-	 */
+    /**
+     * @return mixed
+     */
 
     public function getMsg(){
-		return $this->cfg->get("message");
+	return $this->cfg->get("message");
+    }
+
+    /**
+     * @param $p
+     * @param $name
+     * @return bool
+     */
+
+    public function addDomain($p, $name){
+	$domain = $this->getDomain();
+	if(in_array($name, $domain)){
+	    $p->sendMessage(TF::RED . "That domain already exist!");
+	    return false;
 	}
+	$domain[] = $name;
+	$this->cfg->set("domain", $domain);
+	$this->cfg->save();
+	$p->sendMessage(TF::GREEN . "Successfully added " . $name . " into config");
+	return true;
+    }
+    
+    /**
+     * @param $p
+     * @param $name
+     * @return bool
+     */
 
-	/**
-	 * @param $p
-	 * @param $name
-	 * @return bool
-	 */
+    public function removeDomain($p, $name){
+    	$domain = $this->getDomain();
+    	$key = array_search($name, $domain);
+    	if($key === false){
+    	    $p->sendMessage(TF::RED . "That domain is not exist!");
+    	    return false;
+    	}
+    	unset($domain[$key]);
+    	$this->cfg->set("domain", array_values($domain));
+    	$this->cfg->save();
+    	$p->sendMessage(TF::GREEN . "Successfully removed " . $name . " from config");
+    	return true;
+    }
 
-	public function addDomain($p, $name){
-		$domain = $this->getDomain();
-		if(in_array($name, $domain)){
-			$p->sendMessage(TF::RED . "That domain already exist!");
-			return false;
-		}
-		$domain[] = $name;
-		$this->cfg->set("domain", $domain);
-		$this->cfg->save();
-		$p->sendMessage(TF::GREEN . "Successfully added " . $name . " into config");
-		return true;
+    /**
+     * @param $p
+     * @return bool
+     */
+
+    public function listDomain($p){
+	$domain = implode("\n" . TF::YELLOW . "- ", $this->getDomain());
+	$p->sendMessage(TF::YELLOW . "Available domain:");
+	$p->sendMessage(TF::YELLOW . "- " . $domain);
+	return true;
+    }
+
+    /**
+     * @param $m
+     */
+
+    public function broadcastMsg($m){
+	foreach($this->getServer()->getOnlinePlayers() as $p){
+	    $p->sendMessage($m);
 	}
+    }
 
-	/**
-	 * @param $p
-	 * @param $name
-	 * @return bool
-	 */
+    /**
+     * @return mixed
+     */
 
-	public function removeDomain($p, $name){
-		$domain = $this->getDomain();
-		$key = array_search($name, $domain);
-		if($key === false){
-			$p->sendMessage(TF::RED . "That domain is not exist!");
-			return false;
-		}
-		unset($domain[$key]);
-		$this->cfg->set("domain", array_values($domain));
-		$this->cfg->save();
-		$p->sendMessage(TF::GREEN . "Successfully removed " . $name . " from config");
-		return true;
-	}
-
-	/**
-	 * @param $p
-	 * @return bool
-	 */
-
-	public function listDomain($p){
-		$domain = implode("\n" . TF::YELLOW . "- ", $this->getDomain());
-		$p->sendMessage(TF::YELLOW . "Available domain:");
-		$p->sendMessage(TF::YELLOW . "- " . $domain);
-		return true;
-	}
-
-	/**
-	 * @param $m
-	 */
-
-	public function broadcastMsg($m){
-		foreach($this->getServer()->getOnlinePlayers() as $p){
-			$p->sendMessage($m);
-		}
-	}
-
-	/**
-	 * @return mixed
-	 */
-
-	public function getFormat(){
-		return $this->format;
-	}
+    public function getFormat(){
+	return $this->format;
+    }
 	
-	public function onDisable(){
-		$this->getServer()->getLogger()->info(TF::RED . "NoAdvertisingPE was disabled!");
-	}
+    public function onDisable(){
+	$this->getServer()->getLogger()->info(TF::RED . "NoAdvertisingPE was disabled!");
+    }
 
 }
